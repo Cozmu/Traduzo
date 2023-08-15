be91ee203a5a6e06c9cdc37343bdaceb18a4c997
Cozmu <www.jorgereis@gmail.com>
2023-08-15 15:46:43 -0300

    feat: implementando metodo to_dict para conversão de formato (req 2)

--- src/models/language_model.py
+++ src/models/language_model.py
@@ -2,15 +2,16 @@ from .abstract_model import AbstractModel
 from database.db import db
 
 
-# Req. 1
 class LanguageModel(AbstractModel):
     _collection = db["languages"]
     def __init__(self, data):
         super().__init__(data)
 
-    # Req. 2
     def to_dict(self):
-        raise NotImplementedError
+        return {
+            "name": self.data["name"],
+            "acronym": self.data["acronym"],
+        }
 
     # Req. 3
     @classmethod
