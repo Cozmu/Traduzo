7c508a18727eb01eff2dca0775fa530d30035d3f
Cozmu <www.jorgereis@gmail.com>
2023-08-17 15:58:40 -0300

    chore: retirando comentario

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -7,7 +7,6 @@ from models.language_model import LanguageModel
 translate_controller = Blueprint("translate_controller", __name__)
 
 
-# Reqs. 4 e 5
 @translate_controller.route("/", methods=["GET", "POST"])
 def index():
     languages = LanguageModel.list_dicts()
