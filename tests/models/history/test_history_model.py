932907f7d80ba660adae0201b7be8da683d437dc
Cozmu <www.jorgereis@gmail.com>
2023-08-18 14:15:48 -0300

    test: testes do histórico de traduções (req 7)

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,22 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
-# Req. 7
 def test_request_history():
-    raise NotImplementedError
+    list_dict = json.loads(HistoryModel.list_as_json())
+
+    for item in list_dict:
+        item.pop("_id")
+
+    assert list_dict == [
+        {
+            "text_to_translate": "Hello, I like videogame",
+            "translate_from": "en",
+            "translate_to": "pt",
+        },
+        {
+            "text_to_translate": "Do you love music?",
+            "translate_from": "en",
+            "translate_to": "pt",
+        },
+    ]
